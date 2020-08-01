6daa3c720c8b152d891ed87f5c95f3c231f85146
Aleksipa <paavola.aleksi@gmail.com>
2020-08-01 12:10:09 +0300

    assetNotEqual tests added

--- test/test_brute_force.py
+++ test/test_brute_force.py
@@ -4,9 +4,15 @@ from application.brute_force import bruteForce
 
 class TestBruteForceMethod(unittest.TestCase):
 
-    def test_bruteForce(self):
+    def test_bruteForce_equals(self):
         tries, timeToCrack = bruteForce("ale")
-        self.assertEqual(tries, 97200, "Should be 9039621")
+        self.assertEqual(tries, 97200, "Should be 97200")
+        self.assertAlmostEqual(timeToCrack, 0.0208, places=2)
+    
+    def test_bruteForce_not_equals(self):
+        tries, timeToCrack = bruteForce("ale")
+        self.assertNotEqual(tries, 92200, "Correct is 97200")
+        self.assertNotEqual(timeToCrack, 0.0408)
 
 if __name__ == '__main__':
     unittest.main()
